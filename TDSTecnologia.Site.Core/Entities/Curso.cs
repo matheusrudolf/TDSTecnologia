e86bf2a93e837e079e36cc416b887b32258920b0
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-22 21:20:53 -0400

    Anotações na classe curso representando o banco de dados

--- TDSTecnologia.Site.Core/Entities/Curso.cs
+++ TDSTecnologia.Site.Core/Entities/Curso.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace TDSTecnologia.Site.Core.Entities
 {
+    [Table("tb01_curso")]
     class Curso
     {
+        [Key]
+        [Column("id")]
         public int Id { get; set; }
+
+        [Column("nome")]
         public string Nome { get; set; }
+
+        [Column("descricao")]
         public string Descricao { get; set; }
+
+        [Column("quantidade_aula")]
         public int QuantidadeAula { get; set; }
+
+        [Column("data_inicio")]
         public DateTime DataInicio { get; set; }
     }
 }
