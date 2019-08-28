660b570ccea8be41e4085d335a7de7cbe0b57d0c
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-27 21:38:41 -0400

    Adicionado anotações no enum na classe DomTurno

--- TDSTecnologia.Site.Core/Dominio/DomTurno.cs
+++ TDSTecnologia.Site.Core/Dominio/DomTurno.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TDSTecnologia.Site.Core.Dominio.Extensoes;
 
 namespace TDSTecnologia.Site.Core.Dominio
 {
     public enum DomTurno
     {
+        [EnumDescricao("Matutino")]
         MATUTINO,
+
+        [EnumDescricao("Vespertino")]
         VESPERTINO,
+
+        [EnumDescricao("Noturno")]
         NOTURNO,
+
+        [EnumDescricao("Integral")]
         INTEGRAL
     }
 }
