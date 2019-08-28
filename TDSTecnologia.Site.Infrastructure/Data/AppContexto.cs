7bfe511efd21f31420c65e0dde876fbcff90bc44
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-27 21:27:13 -0400

    Adicionado codigo de conversão na classe AppContexto

--- TDSTecnologia.Site.Infrastructure/Data/AppContexto.cs
+++ TDSTecnologia.Site.Infrastructure/Data/AppContexto.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using TDSTecnologia.Site.Core.Dominio;
 using TDSTecnologia.Site.Core.Entities;
 
 namespace TDSTecnologia.Site.Infrastructure.Data
@@ -13,5 +14,15 @@ namespace TDSTecnologia.Site.Infrastructure.Data
         }
 
         public DbSet<Curso> CursoDao { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+            .Entity<Curso>()
+            .Property(c => c.Turno)
+            .HasConversion(
+            v => v.ToString(),
+            v => (DomTurno)Enum.Parse(typeof(DomTurno), v));
+        }
     }
 }
