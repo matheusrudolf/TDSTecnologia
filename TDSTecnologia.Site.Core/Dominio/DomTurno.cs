81e0ad5020b73677089a7f643eb5b4c9496719ad
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-27 21:10:18 -0400

    Criado a pasta Dominio com a classe DomTurno

---
+++ TDSTecnologia.Site.Core/Dominio/DomTurno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDSTecnologia.Site.Core.Dominio
+{
+    public enum DomTurno
+    {
+        MATUTINO,
+        VESPERTINO,
+        NOTURNO,
+        INTEGRAL
+    }
+}
