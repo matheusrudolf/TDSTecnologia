d1ddfb1a8f6499f0d4ae049eb6070a815907d01a
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-29 19:35:21 -0400

    Criado o Dominio DomModalidade

---
+++ TDSTecnologia.Site.Core/Dominio/DomModalidade.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDSTecnologia.Site.Core.Dominio
+{
+    public enum DomModalidade
+    {
+        EAD,
+        PRESENCIAL
+    }
+}
