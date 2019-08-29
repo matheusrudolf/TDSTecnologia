0bdadafc56c030c5dac9ca93a18612eee2a9fe4d
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-29 19:37:40 -0400

    Correação, adicionado Descrição Enum em Modalidade

--- TDSTecnologia.Site.Core/Dominio/DomModalidade.cs
+++ TDSTecnologia.Site.Core/Dominio/DomModalidade.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TDSTecnologia.Site.Core.Dominio.Extensoes;
 
 namespace TDSTecnologia.Site.Core.Dominio
 {
     public enum DomModalidade
     {
+        [EnumDescricao("EAD")]
         EAD,
+
+        [EnumDescricao("Presencial")]
         PRESENCIAL
     }
 }
