7dcf64279e2e2a45ef9cec418c74fc7f4fe8edc0
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-29 19:44:23 -0400

    Criado o Dominio para Nivel

---
+++ TDSTecnologia.Site.Core/Dominio/DomNivel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TDSTecnologia.Site.Core.Dominio.Extensoes;
+
+namespace TDSTecnologia.Site.Core.Dominio
+{
+    public enum DomNivel
+    {
+        [EnumDescricao("Básico")]
+        BASICO,
+
+        [EnumDescricao("Intermediário")]
+        INTERMEDIARIO,
+
+        [EnumDescricao("Avançado")]
+        AVANÇADO
+    }
+}
