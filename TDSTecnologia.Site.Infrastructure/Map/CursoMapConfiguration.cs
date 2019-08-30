75898d980fba03429e43e093d7e4c38aec8f8387
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-29 22:13:10 -0400

    Criado configuratio para Modalidade e Nivel

--- TDSTecnologia.Site.Infrastructure/Map/CursoMapConfiguration.cs
+++ TDSTecnologia.Site.Infrastructure/Map/CursoMapConfiguration.cs
@@ -23,6 +23,12 @@ namespace TDSTecnologia.Site.Infrastructure.Map
             builder.Property(x => x.Turno)
                 .HasConversion(DominioConverter.ConverterDomTurno());
 
+            builder.Property(x => x.Modalidade)
+                .HasConversion(DominioConverter.ConverterDomModalidade());
+
+            builder.Property(x => x.Nível)
+                .HasConversion(DominioConverter.ConverterDomNivel());
+
             builder.ToTable("tb01_curso");
         }
     }
