c9f428a972ab9b8e29003a3bcb3fc635c7ce3a70
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-29 21:15:01 -0400

    Criado classe UtilImagem em Core, nas pasta Utilitarios, com o metodo ConverterParaByte

---
+++ TDSTecnologia.Site.Core/Utilitarios/UtilImagem.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDSTecnologia.Site.Core.Utilitarios
+{
+    public class UtilImagem
+    {
+        public static async Task<byte[]> ConverterParaByte(IFormFile arquivo)
+        {
+            if (arquivo != null && arquivo.ContentType.ToLower().StartsWith("image/"))
+            {
+                MemoryStream ms = new MemoryStream();
+                await arquivo.OpenReadStream().CopyToAsync(ms);
+                return ms.ToArray();
+            }
+            return null;
+        }
+
+    }
+
+
+}
+
