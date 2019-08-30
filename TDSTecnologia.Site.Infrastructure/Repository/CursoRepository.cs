e58b0cff58f1d84ac6582eaf1c55134b8aa68b5c
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-29 20:52:58 -0400

    Alteração, passado metodo de HomeController para CursoRepository, metodo de listar cursos

--- TDSTecnologia.Site.Infrastructure/Repository/CursoRepository.cs
+++ TDSTecnologia.Site.Infrastructure/Repository/CursoRepository.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using TDSTecnologia.Site.Core.Entities;
 using TDSTecnologia.Site.Infrastructure.Data;
 
 namespace TDSTecnologia.Site.Infrastructure.Repository
@@ -9,7 +12,21 @@ namespace TDSTecnologia.Site.Infrastructure.Repository
     {
         public CursoRepository(AppContexto context) : base(context)
         {
+        }
 
+        public async Task<List<Curso>> ListarTodos()
+        {
+            List<Curso> cursos = await _context.CursoDao.ToListAsync();
+
+            cursos.ForEach(c =>
+            {
+                if (c.Banner != null)
+                {
+                    c.BannerBase64 = "data:image/png;base64," + Convert.ToBase64String(c.Banner, 0, c.Banner.Length);
+                }
+            });
+
+            return cursos;
         }
     }
 }
