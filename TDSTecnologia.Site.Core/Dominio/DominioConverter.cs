a65c1861d5b2dc4b0c0a0ada90b28d474cfcdd31
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-29 22:00:07 -0400

    Criado metodos conversores para Modalidade e Nivel na classe DominioConverter

--- TDSTecnologia.Site.Core/Dominio/DominioConverter.cs
+++ TDSTecnologia.Site.Core/Dominio/DominioConverter.cs
@@ -15,6 +15,22 @@ namespace TDSTecnologia.Site.Core.Dominio
             return converter;
         }
 
+        public static ValueConverter<DomModalidade, string> ConverterDomModalidade()
+        {
+            ValueConverter<DomModalidade, string> converter = new ValueConverter<DomModalidade, string>(
+            v => v.ToString(),
+            v => (DomModalidade)Enum.Parse(typeof(DomModalidade), v));
+            return converter;
+        }
+
+        public static ValueConverter<DomNivel, string> ConverterDomNivel()
+        {
+            ValueConverter<DomNivel, string> converter = new ValueConverter<DomNivel, string>(
+            v => v.ToString(),
+            v => (DomNivel)Enum.Parse(typeof(DomNivel), v));
+            return converter;
+        }
+
     }
     
 }
