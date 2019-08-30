80655dd341ac6588f24f5381d5966c48699c6bb1
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-29 20:49:03 -0400

    Criado a classe CursoRepository herdada de BasicRepository

---
+++ TDSTecnologia.Site.Infrastructure/Repository/CursoRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TDSTecnologia.Site.Infrastructure.Data;
+
+namespace TDSTecnologia.Site.Infrastructure.Repository
+{
+    public class CursoRepository : BasicRepository
+    {
+        public CursoRepository(AppContexto context) : base(context)
+        {
+
+        }
+    }
+}
