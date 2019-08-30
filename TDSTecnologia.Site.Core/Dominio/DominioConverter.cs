37019b8b1a64f56df1b9009fcbb4c8ef3c42add0
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-29 21:45:31 -0400

    Criado a classe DominioConverter

---
+++ TDSTecnologia.Site.Core/Dominio/DominioConverter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDSTecnologia.Site.Core.Dominio
+{
+    public class DominioConverter
+    {
+        public static ValueConverter<DomTurno, string> ConverterDomTurno()
+        {
+            ValueConverter<DomTurno, string> converter = new ValueConverter<DomTurno, string>(
+            v => v.ToString(),
+            v => (DomTurno)Enum.Parse(typeof(DomTurno), v));
+            return converter;
+        }
+
+    }
+    
+}
