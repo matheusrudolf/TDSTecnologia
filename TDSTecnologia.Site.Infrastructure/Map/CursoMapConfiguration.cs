10b9c3dd5bc58d705788ece5de144602e8b4f0d8
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-08-29 22:08:15 -0400

    Criado a pasta Map em Infrastructure, com a classe CursoMapConfiguration

---
+++ TDSTecnologia.Site.Infrastructure/Map/CursoMapConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TDSTecnologia.Site.Core.Dominio;
+using TDSTecnologia.Site.Core.Entities;
+
+namespace TDSTecnologia.Site.Infrastructure.Map
+{
+    public class CursoMapConfiguration : IEntityTypeConfiguration<Curso>
+    {
+        public void Configure(EntityTypeBuilder<Curso> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Nome)
+                .IsRequired()
+                .HasMaxLength(100)
+                .HasColumnType("varchar")
+                .HasColumnName("nome");
+
+            builder.Property(x => x.Turno)
+                .HasConversion(DominioConverter.ConverterDomTurno());
+
+            builder.ToTable("tb01_curso");
+        }
+    }
+}
