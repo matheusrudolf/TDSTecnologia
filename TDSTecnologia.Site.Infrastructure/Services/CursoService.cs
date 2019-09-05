87f87907711cea9783e54f46cc7b2442cc4261c6
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-09-05 19:59:21 -0400

    Criado a classe CursoService na pasta Services

---
+++ TDSTecnologia.Site.Infrastructure/Services/CursoService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TDSTecnologia.Site.Infrastructure.Data;
+using TDSTecnologia.Site.Infrastructure.Repository;
+
+namespace TDSTecnologia.Site.Infrastructure.Services
+{
+    public class CursoService : BasicService
+    {
+        private readonly CursoRepository _cursoRepository;
+
+        public CursoService(AppContexto context) : base(context)
+        {
+            _cursoRepository = new CursoRepository(context);
+        }
+    }
+}
