e3ace50c5175d4d678734d2030cd5458d788084c
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-09-05 21:16:17 -0400

    Criado a classe Permissao na pasta Entities

---
+++ TDSTecnologia.Site.Core/Entities/Permissao.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDSTecnologia.Site.Core.Entities
+{
+    public class Permissao : IdentityRole
+    {
+        public string Descricao { get; set; }
+    }
+}
