17f65516fab92699ebb73638080ccbbee1925468
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-09-05 20:56:11 -0400

    adicionado services CursoService em Startup

--- TDSTecnologia.Site.Web/Startup.cs
+++ TDSTecnologia.Site.Web/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TDSTecnologia.Site.Infrastructure.Data;
 using TDSTecnologia.Site.Infrastructure.Repository;
+using TDSTecnologia.Site.Infrastructure.Services;
 
 namespace TDSTecnologia.Site.Web
 {
@@ -29,6 +30,7 @@ namespace TDSTecnologia.Site.Web
             services.AddMvc();
             services.AddEntityFrameworkNpgsql()
          .AddDbContext<AppContexto>(options => options.UseNpgsql(Configuration.GetConnectionString("AppConnection")));
+            services.AddScoped<CursoService, CursoService>();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
