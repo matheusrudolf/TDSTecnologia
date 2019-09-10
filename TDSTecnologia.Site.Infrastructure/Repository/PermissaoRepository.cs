35871a9a8e7405fe431dfdb55bfe2236a134c056
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-09-10 18:45:43 -0400

    Criada a classe PermissaoRepository na pasta Repositroy

---
+++ TDSTecnologia.Site.Infrastructure/Repository/PermissaoRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TDSTecnologia.Site.Core.Entities;
+using TDSTecnologia.Site.Infrastructure.Data;
+
+namespace TDSTecnologia.Site.Infrastructure.Repository
+{
+    public class PermissaoRepository : BasicRepository
+    {
+        private readonly RoleManager<Permissao> _roleManager;
+
+        public PermissaoRepository(AppContexto contexto, RoleManager<Permissao> roleManager) : base(contexto)
+        {
+            _roleManager = roleManager;
+        }
+    }
+}
