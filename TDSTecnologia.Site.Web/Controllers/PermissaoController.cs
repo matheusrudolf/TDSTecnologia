ac95cc23b598531e4a60845c6aefac437ebf3169
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-09-10 18:59:07 -0400

    Criada a classe PermissaoController na pasta Controller

---
+++ TDSTecnologia.Site.Web/Controllers/PermissaoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TDSTecnologia.Site.Infrastructure.Services;
+
+namespace TDSTecnologia.Site.Web.Controllers
+{
+    public class PermissaoController : Controller
+    {
+        private readonly PermissaoService _permissaoService;
+
+        public PermissaoController(PermissaoService permissaoService)
+        {
+            _permissaoService = permissaoService;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
