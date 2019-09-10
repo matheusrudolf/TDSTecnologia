59bd9950d00a53c35bb05a3d25191faa911892bf
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-09-10 18:52:53 -0400

    Criada a classe PermissaoService na pasta Services

---
+++ TDSTecnologia.Site.Infrastructure/Services/PermissaoService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TDSTecnologia.Site.Core.Entities;
+using TDSTecnologia.Site.Infrastructure.Data;
+using TDSTecnologia.Site.Infrastructure.Repository;
+
+namespace TDSTecnologia.Site.Infrastructure.Services
+{
+    public class PermissaoService : BasicService
+    {
+        private readonly PermissaoRepository _permissaoRepository;
+
+        public PermissaoService(AppContexto contexto, RoleManager<Permissao> roleManager) : base(contexto)
+        {
+            _permissaoRepository = new PermissaoRepository(contexto, roleManager);
+        }
+    }
+}
