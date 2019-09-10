94a769246b396af90e8eb47c5a6ca1e434e6f5d7
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-09-10 19:10:11 -0400

    Nenhuma alt

--- TDSTecnologia.Site.Infrastructure/Repository/PermissaoRepository.cs
+++ TDSTecnologia.Site.Infrastructure/Repository/PermissaoRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using TDSTecnologia.Site.Core.Entities;
 using TDSTecnologia.Site.Infrastructure.Data;
