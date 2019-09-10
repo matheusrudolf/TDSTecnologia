d2dadbdc1ae27cb5bb158639bf628099c0ff57fd
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-09-10 19:48:10 -0400

    Adicionado metodos de Exclusao em PermissaoService

--- TDSTecnologia.Site.Infrastructure/Services/PermissaoService.cs
+++ TDSTecnologia.Site.Infrastructure/Services/PermissaoService.cs
@@ -34,5 +34,16 @@ namespace TDSTecnologia.Site.Infrastructure.Services
         {
             return await _permissaoRepository.ExistePermissao(permissao);
         }
+
+        public Permissao PesquisarPorId(string id)
+        {
+            return _permissaoRepository.PesquisarPorId(id);
+        }
+
+        public void Excluir(string id)
+        {
+            _permissaoRepository.Excluir(PesquisarPorId(id));
+            SaveChangesApp();
+        }
     }
 }
