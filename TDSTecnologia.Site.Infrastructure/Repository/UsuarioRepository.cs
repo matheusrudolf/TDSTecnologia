92db72563d2bf5e8d7a399acffd42f46d8ad03d9
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-09-10 21:31:19 -0400

    Criada a classe UsuarioRepository na pasta Repository

---
+++ TDSTecnologia.Site.Infrastructure/Repository/UsuarioRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TDSTecnologia.Site.Core.Entities;
+using TDSTecnologia.Site.Infrastructure.Data;
+
+namespace TDSTecnologia.Site.Infrastructure.Repository
+{
+    public class UsuarioRepository : BasicRepository
+    {
+        private readonly SignInManager<Usuario> _signInManager;
+        private readonly UserManager<Usuario> _userManager;
+
+        public UsuarioRepository(AppContexto context, UserManager<Usuario> userManager, SignInManager<Usuario> signInManager) : base(context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+    }
+}
