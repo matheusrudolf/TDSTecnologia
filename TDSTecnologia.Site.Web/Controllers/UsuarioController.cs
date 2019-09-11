baac03b7e481ca109c0e7af4164ea003edd71178
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-09-10 21:45:45 -0400

    Criado a classe UsuarioController na pasta Controller

---
+++ TDSTecnologia.Site.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TDSTecnologia.Site.Infrastructure.Services;
+
+namespace TDSTecnologia.Site.Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly UsuarioService _usuarioService;
+        public UsuarioController(UsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+    }
+}
